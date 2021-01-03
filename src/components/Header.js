e3365c59e1facbfa7ed6d8f03cbac6571c8fe1a9
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-01-03 10:30:26 +0000

    home page update

---
+++ src/components/Header.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './css/Header.scss'
+import Logo from "../assets/logo.png";
+const Header = () => {
+    return (
+        <>
+        <header>
+       
+        <div className="intro">
+        
+          <div className="overlay">
+            <div className="container">
+              <div className="row">
+                <div className="col-md-8 col-md-offset-2 intro-text">
+                <img src={Logo} alt="logo" className = 'header-logo' />
+                  <h1>
+                    WE are Jambo Tree
+                    <span></span>
+                  </h1>
+                  <p>
+                  Exporters of African exotic indigenous tree seedlings, fruits & vegetables 
+                  </p> 
+                  <p>&</p>
+                  <p>
+                  WORLDWIDE EXPORT OF MATURE BAOBAB TREES 
+                  </p>
+                  <button
+                    href="#features"
+                    className="btn btn-custom"
+                  >
+                    Learn More
+                  </button>{" "}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </header>
+            
+        </>
+        
+    )
+}
+
+export default Header
