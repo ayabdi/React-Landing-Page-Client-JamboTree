009d2a45e930a9f7149d95c77429cc79b6a2ee24
ayabdi <46926470+ayabdi@users.noreply.github.com>
2021-01-07 11:58:31 +0000

    product image hover responsive

--- src/components/Products.js
+++ src/components/Products.js
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import JsonData from "../data.json";
 import "./css/Products.scss";
 import TreeSeedling from "../assets/treeseedling1.jpg";
-import MatureTree from '../assets/maturetree.jpg'
-import Flowers from '../assets/flowers.jpg';
-import Vegetables from '../assets/vegetables.jfif'
-import Spices from '../assets/spices.jpg'
+import MatureTree from "../assets/maturetree.jpg";
+import Flowers from "../assets/flowers.jpg";
+import Vegetables from "../assets/vegetables.jfif";
+import Spices from "../assets/spices.jpg";
 const Products = () => {
   const [product] = useState(JsonData.Products);
 
@@ -23,47 +23,75 @@ const Products = () => {
         <div className="row">
           <div className="col-md-4">
             {" "}
-            <img src={TreeSeedling} ></img>
-            <div className="service-desc">
-              <h3>{product[0].name}</h3>
-              <p>{product[0].text}</p>
+            <div className="hover-bg">
+              <a href = '#' title='Project Title' data-lightbox-gallery ='gallery1'>
+              <div className="hover-text">
+                <h4>Read More</h4>
+              </div>
+              <img src={TreeSeedling} className='img-responsive'></img>
+             
+              </a>
+              <div className="service-desc">
+                <h3>{product[0].name}</h3>
+                <p>{product[0].text}</p>
+              </div>
             </div>
           </div>
           <div className="col-md-4">
             {" "}
-            <img src={MatureTree} ></img>
-            <div className="service-desc">
-              <h3>{product[1].name}</h3>
-              <p>{product[1].text}</p>
+            <div className="hover-bg">
+              <div className="hover-text">
+                <h4>Read More</h4>
+              </div>
+              <img src={MatureTree}></img>
+              <div className="service-desc">
+                <h3>{product[1].name}</h3>
+                <p>{product[1].text}</p>
+              </div>
             </div>
           </div>
           <div className="col-md-4">
             {" "}
-            <img src={Flowers} ></img>
-            <div className="service-desc">
-              <h3>{product[2].name}</h3>
-              <p>{product[2].text}</p>
+            <div className="hover-bg">
+              <div className="hover-text">
+                <h4>Read More</h4>
+              </div>
+              <img src={Flowers}></img>
+              <div className="service-desc">
+                <h3>{product[2].name}</h3>
+                <p>{product[2].text}</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="row" style ={{justifyContent: 'center' }}>
-            <div className="col-md-4">
-              {" "}
-              <img src={Vegetables} ></img>
+        <div className="row" style={{ justifyContent: "center" }}>
+          <div className="col-md-4">
+            {" "}
+            <div className="hover-bg">
+              <div className="hover-text">
+                <h4>Read More</h4>
+              </div>
+              <img src={Vegetables}></img>
               <div className="service-desc">
                 <h3>{product[3].name}</h3>
                 <p>{product[3].text}</p>
               </div>
             </div>
-            <div className="col-md-4">
-              {" "}
-              <img src={Spices} ></img>
+          </div>
+          <div className="col-md-4">
+            {" "}
+            <div className="hover-bg">
+              <div className="hover-text">
+                <h4>Read More</h4>
+              </div>
+              <img src={Spices}></img>
               <div className="service-desc">
                 <h3>{product[4].name}</h3>
                 <p>{product[4].text}</p>
               </div>
             </div>
           </div>
+        </div>
       </div>
     </div>
   );
